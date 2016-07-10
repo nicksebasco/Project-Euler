91d1de78cbddcb576cba2a8933ee20350d7341fc
nicksebasco <nick.sebasco@ttu.edu>
2016-07-10 15:43:57 -0500

    Add files via upload

---
+++ p20.py
@@ -0,0 +1,18 @@
+# calculate sum of digits in 100!
+
+def fact(n):
+	total = 1
+	while n > 0:
+		total = total * n
+		n = n - 1
+	return total
+
+def sum_digits(n):
+	return sum([int(i) for i in list(str(n))])
+
+def main():
+	f100 = fact(100)
+	sd = sum_digits(f100)
+	print sd
+
+main()
