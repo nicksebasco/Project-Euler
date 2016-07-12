c2a290f884ad3be9e266c7c78eb23eef226833e4
nicksebasco <nick.sebasco@ttu.edu>
2016-07-11 23:35:04 -0500

    Update p20.py

--- p20.py
+++ p20.py
@@ -2,18 +2,20 @@
 # solution is trivial in python
 
 def fact(n):
-	total = 1
-	while n > 0:
-		total = total * n
-		n = n - 1
-	return total
+	return n*fact(n-1) if n>1 else 1
 
 def sum_digits(n):
 	return sum([int(i) for i in list(str(n))])
-
+	
 def main():
 	f100 = fact(100)
 	sd = sum_digits(f100)
 	print sd
-
 main()
+
+"""
+# alternatively
+fact = lambda n: n*fact(n-1) if n>1 else 1
+sum_digits = lambda n: sum([int(i) for i in list(str(n))])
+print sum_digits(fact(100))
+"""
